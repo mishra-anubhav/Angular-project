b9d6b116a9dff7d9791466c9cd29fa7d8744bfa3
anubhavmishra1 <39110231+anubhavmishra1@users.noreply.github.com>
2019-06-10 17:10:20 +0530

    Project-6-and-7-updated

--- Project 7 Angular JumpStart/src/app/app-routing.module.ts
+++ Project 7 Angular JumpStart/src/app/app-routing.module.ts
@@ -5,10 +5,10 @@ import { PreloadModulesStrategy } from './core/strategies/preload-modules.strate
 
 const app_routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/customers' },
-  { path: 'customers/:id', data: { preload: true }, loadChildren: 'app/customer/customer.module#CustomerModule' },
-  { path: 'customers', loadChildren: 'app/customers/customers.module#CustomersModule' },
-  { path: 'orders', data: { preload: true }, loadChildren: 'app/orders/orders.module#OrdersModule' },
-  { path: 'about', loadChildren: 'app/about/about.module#AboutModule' },
+  { path: 'customers/:id', data: { preload: true }, loadChildren: '../customer/customer.module#CustomerModule' },
+  { path: 'customers', loadChildren: '../customers/customers.module#CustomersModule' },
+  { path: 'orders', data: { preload: true }, loadChildren: '../orders/orders.module#OrdersModule' },
+  { path: 'about', loadChildren: '../about/about.module#AboutModule' },
   { path: '**', pathMatch: 'full', redirectTo: '/customers' } // catch any unfound routes and redirect to home page
 ];
 
